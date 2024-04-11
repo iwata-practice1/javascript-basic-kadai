06372ba94bb22a5049771afc245353040f1335b5
iwata-practice1 <iwamari@hi.enjoy.ne.jp>
2024-04-11 10:50:25 +0900

    kadai_020を追加

---
+++ kadai_020/event.js
@@ -0,0 +1,9 @@
+console.log(document.head);
+console.log(document.body);
+console.log(window);
+
+const btn = document.getElementById('btn');
+const text = document.getElementById('text');
+btn.addEventListener('click', () => {
+  text.textContent = 'ボタンをクリックしました';
+});
